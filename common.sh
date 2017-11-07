c58ab2cf08819fd90f46f68418e4b16d558640d7
Murilo Belluzzo <murilo.belluzzo@intel.com>
2017-11-07 12:10:06 -0800

    Add script stubs for the release process
    The goal is to have one folder per Jenkins job containing the pipeline
    file and the scripts needed by that job.
    
    Also, try keep most (if not all) the logic inside the scripts and not on the
    Jenkins. Trying to make the server just a script orchestrator.
    
    samplejob/
        SampleJobPipeline
        step_script.sh
    
    Signed-off-by: Murilo Belluzzo <murilo.belluzzo@intel.com>

---
+++ common.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+# Copyright (c) 2017 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+test_dep () {
+    command -v $1 > /dev/null 2>&1 || { echo >&2 "Error: command '$1' not found"; exit 1; }
+}
+
+test_dir () {
+    [ -d $1 ] > /dev/null 2>&1 || { echo >&2 "Error: directory '$1' not found"; exit 1; }
+}
+
+silentkill () {
+    if [ ! -z $2 ]; then
+        kill $2 $1 > /dev/null 2>&1 || true
+    else
+        kill -KILL $1 > /dev/null 2>&1 || true
+    fi
+}
+
+run_and_log() {
+    cmd=$1
+    log_out="${2}.log"
+    log_err="${2}_err.log"
+
+    # Log to file
+    if [ "$VERBOSE_LEVEL" -eq 0 ]; then
+        $cmd > "${LOGDIR}/${log_out}" 2> "${LOGDIR}/${log_err}" &
+    # Log to file and stdout
+    elif [ "$VERBOSE_LEVEL" -eq 1 ]; then
+        $cmd > >(tee "${LOGDIR}/${log_out}") 2> >(tee "${LOGDIR}/${log_err}") &
+    # Log to stdout only
+    elif [ "$VERBOSE_LEVEL" -eq 2 ]; then
+        $cmd &
+    # No output
+    else
+        $cmd > /dev/null 2> /dev/null &
+    fi
+}
