def7cb0a2b6259528f1f074484f2432c7dbde304
Mark Horn <mark.d.horn@intel.com>
2017-11-30 16:09:00 -0800

    [release] Add code to create the OS image of a Mix
    Signed-off-by: Mark Horn <mark.d.horn@intel.com>

--- release/ister.sh
+++ release/ister.sh
@@ -13,4 +13,50 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-echo "Hello Ister"
+set -e
+
+SCRIPT_DIR=$(dirname $(realpath ${BASH_SOURCE[0]}))
+
+. ${SCRIPT_DIR}/../globals.sh
+. ${SCRIPT_DIR}/../common.sh
+
+BUILDER_CONF=${BUILDER_CONF:-"${SCRIPT_DIR}/builder.conf"}
+IMAGE_TEMPLATE=${IMAGE_TEMPLATE:-"${SCRIPT_DIR}/release-image-config.json"}
+
+update_image_template() {
+    bundles=""
+    for bundle in $(/usr/bin/ls mix-bundles); do
+        if [ ! -z "${bundles}" ]; then
+            bundles+=", "
+        fi
+        bundles+="\"${bundle}\""
+    done
+    bundles="\"Bundles\": [${bundles}]"
+
+    cat ${IMAGE_TEMPLATE} | sed "s/\"Bundles\":.*/${bundles}/" > ${IMAGE_TEMPLATE}.new
+    /usr/bin/mv ${IMAGE_TEMPLATE} ${IMAGE_TEMPLATE}.$(date +%Y%m%d.%H%M%S)
+    /usr/bin/mv ${IMAGE_TEMPLATE}.new ${IMAGE_TEMPLATE}
+}
+
+main() {
+    test_dir ${BUILD_DIR}
+    pushd ${BUILD_DIR} > /dev/null
+
+    echo ""
+    echo "Generating image template ..."
+    update_image_template
+
+    echo ""
+    echo "Generating release image  ..."
+    CURRENT_FORMAT=$(grep '^FORMAT' ${BUILDER_CONF} | awk -F= '{print $NF}')
+    # Requires mixer 3.1.2 (https://github.com/mdhorn/mixer-tools.git integration)
+    sudo -E mixer build-image -config ${BUILDER_CONF} -template ${IMAGE_TEMPLATE} -format ${CURRENT_FORMAT}
+
+    mixversion=$(cat .mixversion)
+    mkdir -p releases
+    sudo -E /usr/bin/mv release.img releases/release-${mixversion}.img
+
+    popd > /dev/null
+}
+
+main
