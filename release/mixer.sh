905f5d6fb68b0772608ac794924e0562693e0aa5
Caio Marcelo de Oliveira Filho <caio.oliveira@intel.com>
2017-12-08 12:57:07 -0800

    [release] Always use Clear Linux as upstream for now
    The files we with upstream information are not being added yet to the
    "www" data, so not available to use. And more importantly there is
    still not clarity on how to handle certain cases in that scenario,
    e.g. what if we want to change the upstream URL for a new release?
    
    So fallback to use upstream as always Clear Linux for now, and we add
    the capability of different upstreams on top.
    
    The upstream information will be generated as part of
    https://github.com/clearlinux/mixer-tools/pull/31.
    
    This patch was based on patch by Murilo.
    
    Signed-off-by: Caio Marcelo de Oliveira Filho <caio.oliveira@intel.com>

--- release/mixer.sh
+++ release/mixer.sh
@@ -37,6 +37,8 @@ BUILDER_CONF=${BUILDER_CONF:-"${BUILD_DIR}/builder.conf"}
 MIX_INCREMENT=${MIX_INCREMENT:-10}
 MIX_BUNDLES_URL=${MIX_BUNDLES_URL:?"Downstream Bundles Repository is required"}
 
+UPSTREAM_URL=${UPSTREAM_URL:-"${CLR_PUBLIC_DL_URL}/update/"}
+
 # Comma (no whitespace) separated list of bundles to include
 # Leave zero length string for all ClearLinux bundles (Default mode)
 INC_BUNDLES=${INC_BUNDLES:-"bootloader,kernel-native,os-core,os-core-update"}
@@ -44,31 +46,6 @@ INC_BUNDLES=${INC_BUNDLES:-"bootloader,kernel-native,os-core,os-core-update"}
 # URL of RPM download site
 KOJI_TOPURL=${KOJI_TOPURL:?"Downstream Koji Top URL is required"}
 KOJI_RPM_TAG=${KOJI_RPM_TAG:-"dist-ds"} # Koji tag of RPMs to use
-
-get_upstream_prev_info() {
-    # First check if we have a local, unpublished Mix
-    if [ -d "update/${MIX_LATEST_VERSION}" ]; then
-        { # Get the Upstream version and format for the previous mix
-            UPSTREAM_PREV_VERSION=$(cat update/${MIX_LATEST_VERSION}/upstream_ver) &&
-            UPSTREAM_PREV_FORMAT=$(curl --silent --fail ${UPSTREAM_URL}update/${UPSTREAM_PREV_VERSION}/format)
-        } || { # Failed
-            echo "[local] Failed to get Upstream previous ClearLinux version information!"
-            exit 2
-        }
-    else
-        { # Get the Upstream version and format for the previous mix
-            UPSTREAM_PREV_VERSION=$(curl --silent --fail file://${STAGING_DIR}/update/${MIX_LATEST_VERSION}/upstream_ver) &&
-            UPSTREAM_PREV_FORMAT=$(curl --silent --fail ${UPSTREAM_URL}${UPSTREAM_PREV_VERSION}/format)
-        } || { # Failed
-            echo "Failed to get Upstream previous ClearLinux version information!"
-            exit 2
-        }
-    fi
-
-    if [ ${VERBOSE} ]; then
-        echo "Upstream previous ClearLinux Version: ${UPSTREAM_PREV_VERSION}:${UPSTREAM_PREV_FORMAT}"
-    fi
-}
 
 get_latest_versions() {
     {
@@ -78,19 +55,16 @@ get_latest_versions() {
         MIX_LATEST_VERSION=""
     }
 
+    {
+        UPSTREAM_BASE_VERSION=${UPSTREAM_BASE_VERSION:-$(curl --silent --fail ${UPSTREAM_URL}../latest)} &&
+        UPSTREAM_BASE_FORMAT=$(curl --silent --fail ${UPSTREAM_URL}${UPSTREAM_BASE_VERSION}/format)
+    } || {
+        echo "Failed to get ClearLinux upstream latest version information!"
+        exit 4
+    }
+
     if [ -z ${MIX_LATEST_VERSION} ]; then
-        # This is the First Mix, fallback to the Public upstream
-        echo "INFO: No latest Mix, assume this is the first Mix..."
-
-        UPSTREAM_URL="${CLR_PUBLIC_DL_URL}/update/"
-        {
-            UPSTREAM_BASE_VERSION=${UPSTREAM_BASE_VERSION:-$(curl --silent --fail ${UPSTREAM_URL}../latest)} &&
-            UPSTREAM_BASE_FORMAT=$(curl --silent --fail ${UPSTREAM_URL}${UPSTREAM_BASE_VERSION}/format)
-        } || { # Failed
-            echo "Failed to get ClearLinux (public) upstream latest version information!"
-            exit 4
-        }
-
+        echo "INFO: No latest Mix, building the first mix"
         MIX_VERSION=${MIX_VERSION:-$(( ${UPSTREAM_BASE_VERSION} * 1000 + ${MIX_INCREMENT}))}
 
         MIX_LATEST_VERSION=${MIX_VERSION}
@@ -99,25 +73,29 @@ get_latest_versions() {
         UPSTREAM_PREV_VERSION=${UPSTREAM_BASE_VERSION}
         UPSTREAM_PREV_FORMAT=${UPSTREAM_BASE_FORMAT}
     else
+        echo "INFO: Latest mix is ${MIX_LATEST_VERSION}, building"
+
         { # Get Latest Mix version's format (we already have the version)
             MIX_LATEST_FORMAT=$(curl --silent --fail file://${STAGING_DIR}/update/${MIX_LATEST_VERSION}/format)
         } || { # Failed
-            echo "Failed to get Latest Mix ClearLinux version information!"
+            echo "Failed to get latest mix format!"
             exit 2
-        }
-
-        {
-            UPSTREAM_URL=${UPSTREAM_URL:-$(curl --silent --fail file://${STAGING_DIR}/update/${MIX_LATEST_VERSION}/upstream_url)} &&
-            UPSTREAM_BASE_VERSION=${UPSTREAM_BASE_VERSION:-$(curl --silent --fail ${UPSTREAM_URL}../latest)} &&
-            UPSTREAM_BASE_FORMAT=$(curl --silent --fail ${UPSTREAM_URL}${UPSTREAM_BASE_VERSION}/format)
-        } || { # Failed
-            echo "Failed to get ClearLinux upstream version information!"
-            exit 4
         }
 
         MIX_VERSION=${MIX_VERSION:-$(( ${UPSTREAM_BASE_VERSION} * 1000 + ${MIX_INCREMENT}))}
 
-        get_upstream_prev_info
+        UPSTREAM_PREV_VERSION=${MIX_LATEST_VERSION::-3}
+
+        { # Get the Upstream version and format for the previous mix
+            UPSTREAM_PREV_FORMAT=$(curl --silent --fail ${UPSTREAM_URL}${UPSTREAM_PREV_VERSION}/format)
+        } || { # Failed
+            echo "Failed to get Upstream previous ClearLinux version information!"
+            exit 2
+        }
+
+        if [ ${VERBOSE} ]; then
+            echo "Upstream previous ClearLinux Version: ${UPSTREAM_PREV_VERSION}:${UPSTREAM_PREV_FORMAT}"
+        fi
 
         if [ ${MIX_VERSION} -le ${MIX_LATEST_VERSION} ]; then
             echo "New Mix Version ${MIX_VERSION} is than or equal to Latest Mix Version ${MIX_LATEST_VERSION}!"
@@ -127,7 +105,7 @@ get_latest_versions() {
     fi
 
     if [ ${VERBOSE} ]; then
-        echo "Mixer: ${MIX_VERSION}"
+        echo "Mix version to create: ${MIX_VERSION}"
         echo "Mix Latest Version: ${MIX_LATEST_VERSION}:${MIX_LATEST_FORMAT}"
         echo "Upstream Version as Base for new Mix: ${UPSTREAM_BASE_VERSION}:${UPSTREAM_BASE_FORMAT}"
     fi
