d1673382b9251fa98fd805871ca6fb1c55155247
George T Kramer <george.t.kramer@intel.com>
2018-05-03 15:05:59 -0700

    [release] Publish staged content
    Set up a publishing routine for further expansion later.  Publishing
    works per staging directory to make it namespace-aware.
    
    Signed-off-by: George T Kramer <george.t.kramer@intel.com>

---
+++ release/publish.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# Copyright (C) 2018 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+set -e
+
+SCRIPT_DIR=$(dirname $(realpath ${BASH_SOURCE[0]}))
+
+. ${SCRIPT_DIR}/../common.sh
+. ./config/config.sh
+
+REMOTE_PATH=${PUBLISHING_HOST}:${PUBLISHING_ROOT}/${NAMESPACE:-${DSTREAM_NAME}}
+
+cat <<EOL
+=== PUBLISH
+From:
+    ${STAGING_DIR}
+To:
+    ${REMOTE_PATH}
+
+== Syncing Content ==
+
+EOL
+
+assert_dir ${STAGING_DIR}
+rsync -rlHpt --delete --exclude '*.src.rpm' -e ssh ${STAGING_DIR}/ ${REMOTE_PATH}
