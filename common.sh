5f9166c791569399dcfabc0107d1da1f2db18281
Murilo Belluzzo <murilo.belluzzo@intel.com>
2018-05-30 19:18:17 -0700

    [common] Add helper functions for logging
    It is aimed to help produce the output pattern described bellow. It
    allows users to set a domain via LOG_DOMAIN and a default indentation
    level via LOG_INDENT. One can also set additional indentation for
    individual calls.
    
    It also add helper functions to log errors, warnings and extra info.
    
    Output Pattern:
    
    === STAGE
    <empty line>
    == Section == <dependent on the script's logic>
    Log Title:
        Log Message (4 space indentation)
    Log Another Title: ( <= your eyes find this easier)
        Log Another Message ( as well this once you find what you were looking for)
    <empty line>
    \# Keep empty lines around commands that have verbose output
    verbose command output here.
    <empty line>
    Log Yet Another Title:
        So this is still easy for humans to find in the log
    
    Signed-off-by: Murilo Belluzzo <murilo.belluzzo@intel.com>

--- common.sh
+++ common.sh
@@ -13,6 +13,57 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# =================
+# Logging Utilities
+# =================
+
+# LOG_DOMAIN = Domain name to be printed in the beginning of each log line
+# LOG_INDENT = Indentation level to be applied on the log message after 'domain'
+
+log_line() {
+    # ${1} - log message
+    # ${2} - extra indentation
+
+    local indent=${1:+$(( (${LOG_INDENT:-0} + ${2:-0}) * 4 ))}
+    printf "${LOG_DOMAIN:+"[${LOG_DOMAIN}] "}%${indent}s%s\n" "" "${1}"
+}
+
+log() {
+    # ${1} - log title (or message for single line logs)
+    # ${2} - log message (optional)
+    # ${3} - extra indentation (optional, requires ${2})
+
+    if (( $# < 2 )); then
+        log_line "${1}"
+    else
+        log_line "${1}:" ${3}
+        log_line "${2}" $((${3:-0} + 1))
+    fi
+}
+
+stage() {
+    log_line "=== ${1^^}"
+    log_line
+}
+
+section() {
+    log_line "== ${1} =="
+}
+
+error() {
+    log "[ERROR] ${1}" ${2:+"${2}"} ${3}
+}
+
+info() {
+    log "[INFO] ${1}" ${2:+"${2}"} ${3}
+}
+
+warn() {
+    log "[WARN] ${1}" ${2:+"${2}"} ${3}
+}
+
+# =================
+
 curl() {
     command curl --silent --fail $@
 }
