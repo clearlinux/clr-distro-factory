ed597a4dc9f4da824c0cbc48630d7e8059e9b2fc
Vinay Potluri <vinay.potluri@intel.com>
2018-06-13 11:10:06 -0700

    [koji] Adding Koji Pipeline
    This Pipeline is created to execute any koji related tasks.
    update_external_repo script looks for the latest release and
    updates external repo url in koji
    
    Signed-off-by: Vinay Potluri <vinay.potluri@intel.com>

---
+++ koji/update_external_repo.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# Copyright (C) 2018 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+set -e
+
+SCRIPT_DIR=$(dirname $(realpath ${BASH_SOURCE[0]}))
+
+. ${SCRIPT_DIR}/../globals.sh
+. ${SCRIPT_DIR}/../common.sh
+
+KOJI_TAG=${KOJI_TAG:-"dist-clear"}
+repo_name="dist-clear-external-repo"
+repo_prefix="${CLR_PUBLIC_DL_URL}/releases/"
+repo_suffix="/clear/\$arch/os/"
+
+current_version=$(koji list-external-repos --name=${repo_name} --quiet)
+current_version=${current_version##*releases/}
+current_version=${current_version%%${repo_suffix}}
+
+get_upstream_version
+
+(( ${CLR_LATEST} <= ${current_version} )) && exit 0
+
+koji edit-external-repo --url="${repo_prefix}${CLR_LATEST}${repo_suffix}" ${repo_name}
+koji regen-repo ${KOJI_TAG}-build
