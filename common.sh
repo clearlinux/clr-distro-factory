74db42c82d8f49377b5cad3128be60da52c553bd
Murilo Belluzzo <murilo.belluzzo@intel.com>
2018-10-16 17:07:21 -0700

    [common] Match upstream format for first mix
    Only default to '1' when we don't have an upstream.
    
    Signed-off-by: Murilo Belluzzo <murilo.belluzzo@intel.com>

--- common.sh
+++ common.sh
@@ -201,7 +201,7 @@ get_downstream_version() {
     DS_LATEST=$(cat ${STAGING_DIR}/latest 2>/dev/null) || true
     if [[ -z $DS_LATEST ]]; then
         info "Failed to fetch Downstream latest version. First Mix?"
-        DS_FORMAT=1
+        DS_FORMAT=${CLR_FORMAT:-1}
     elif ((${#DS_LATEST} < 4)); then
         error "Downstream Clear Linux version number seems corrupted."
         exit 2
