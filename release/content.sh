5804049f2d06357ea97fa0b7104da1ef079a0827
Murilo Belluzzo <murilo.belluzzo@intel.com>
2018-10-23 08:36:17 -0700

    [content] Fix package list when there is no custom content
    It was exiting too early so we didn't 'touch' the packages file.
    
    Signed-off-by: Murilo Belluzzo <murilo.belluzzo@intel.com>

--- release/content.sh
+++ release/content.sh
@@ -16,19 +16,19 @@ SCRIPT_DIR=$(dirname $(realpath ${BASH_SOURCE[0]}))
 # ==============================================================================
 stage "Finalizing Content"
 
-if [[ -z "$(ls -A ${PKGS_DIR})" ]]; then
-    info "Custom Content Not Found" " '${PKGS_DIR}' is empty."
-    exit 0
-fi
-
 log_line "Building package list:"
 # If no content providers fetched packages, this will act as 'touch'
 cat ${WORK_DIR}/${PKG_LIST_TMP}* > ${WORK_DIR}/${PKG_LIST_FILE} 2>/dev/null || true
 log_line "OK!" 1
 
 section "Creating Content Repository"
+if [[ -z "$(ls -A ${PKGS_DIR})" ]]; then
+    info "Custom Content Not Found" " '${PKGS_DIR}' is empty."
+    exit 0
+fi
+
 pushd ${PKGS_DIR} > /dev/null
-log_line
-createrepo_c ${PKGS_DIR} # Output too verbose
+log_line # Output too verbose
+createrepo_c ${PKGS_DIR} # TODO: log create_repo output and only print its result
 log_line
 popd > /dev/null
