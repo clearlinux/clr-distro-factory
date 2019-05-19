9bab1026fa050135073a769a9728d3cd0b2c25a3
Murilo Belluzzo <murilo.belluzzo@intel.com>
2019-05-19 16:26:10 -0700

    [watcher] Revamp logging output
    Signed-off-by: Murilo Belluzzo <murilo.belluzzo@intel.com>

--- watcher/watcher.sh
+++ watcher/watcher.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Copyright (C) 2018 Intel Corporation
+#
 # SPDX-License-Identifier: Apache-2.0
 
 # shellcheck source=common.sh
@@ -15,30 +16,30 @@ SCRIPT_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
 
 . "${SCRIPT_DIR}/../common.sh"
 
-echo "=== Watcher"
+# ==============================================================================
+# MAIN
+# ==============================================================================
+stage "Watcher"
 fetch_config_repo
 . ./config/config.sh
 
-echo "Upstream Server:"
-echo "    ${CLR_PUBLIC_DL_URL}"
-echo "Downstream Stage:"
-echo "    ${STAGING_DIR}"
-echo
+log "Upstream Server" "${CLR_PUBLIC_DL_URL}"
+log "Downstream Stage" "${STAGING_DIR}"
+log_line
 
 # Check if we are on track with Upstream ClearLinux
 get_latest_versions
 
-echo "Clear Linux version:"
-echo "    $CLR_LATEST"
-echo "Downstream version:"
+log "Clear Linux version" "${CLR_LATEST}"
+log "Downstream version"
 if [[ -z $DS_LATEST ]]; then
-    echo "    First Mix! It's Release Time!"
+    log_line "First Mix! It's Release Time!" 1
     exit 1
 fi
-echo "    $DS_UP_VERSION $DS_DOWN_VERSION"
+log_line "${DS_UP_VERSION} ${DS_DOWN_VERSION}" 1
 
 if (( DS_UP_VERSION < CLR_LATEST )); then
-    echo "Upstream has a new release. It's Release Time!"
+    log "Upstream has a new release" "It's Release Time!"
     exit 1
 fi
 
@@ -46,23 +47,23 @@ fi
 ret=0
 TMP_PREV_LIST=$(mktemp)
 TMP_CURR_LIST=$(mktemp)
-PKG_LIST_PATH=${STAGING_DIR}/releases/${DS_LATEST}/${PKG_LIST_FILE}-${DS_LATEST}.txt
+PKG_LIST_PATH="${STAGING_DIR}/releases/${DS_LATEST}/${PKG_LIST_FILE}-${DS_LATEST}.txt"
 
 if ! cat "${PKG_LIST_PATH}" > "${TMP_PREV_LIST}"; then
-    echo "Wrn: Failed to fetch Downstream PREVIOUS Package List. Assuming empty."
+    warn "Failed to fetch Downstream PREVIOUS Package List" "Assuming empty"
 fi
 
 if result=$(koji_cmd list-tagged --latest --quiet "${KOJI_TAG}"); then
     echo "${result}" | awk '{print $1}' > "${TMP_CURR_LIST}"
 else
-    echo "Wrn: Failed to fetch Downstream Package List. Assuming empty."
+    warn "Failed to fetch Downstream Package List" "Assuming empty."
 fi
 
 if ! diff "${TMP_CURR_LIST}" "${TMP_PREV_LIST}"; then
-    echo "New custom content. It's Release Time!"
+    log "New custom content" "It's Release Time!"
     ret=1
 else
-    echo "Nothing to see here."
+    log "Nothing to see here."
 fi
 
 rm "${TMP_CURR_LIST}"
