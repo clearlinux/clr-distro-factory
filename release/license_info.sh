12d8201f07ce027bdf88ecfcf5d465e7ff076d2a
Tan, Yew Wayne <yew.wayne.tan@intel.com>
2019-06-04 16:17:25 +0800

    [stage] Generate license info
    List out all licenses used by all packages in the mixversion
    
    Signed-off-by: Tan, Yew Wayne <yew.wayne.tan@intel.com>

---
+++ release/license_info.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# Copyright (C) 2018 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+# shellcheck source=common.sh
+
+set -e
+
+SCRIPT_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+. "${SCRIPT_DIR}/../common.sh"
+. ./config/config.sh
+
+var_load MIX_VERSION
+var_load CLR_LATEST
+dnf_conf="${MIXER_DIR}/.yum-mix.conf"
+
+generate_license_info() {
+    awk '{print $1}' < "${MIXER_DIR}/update/image/${MIX_VERSION}/os-packages" |
+        xargs dnf --quiet --config "${dnf_conf}" --releasever="${CLR_LATEST}" --installroot=dnf-cache repoquery --queryformat "%{LICENSE}" |
+        tr ' ' '\n' |
+        sort -u
+}
+
+log "Generating License Info"
+generate_license_info > "${WORK_DIR}/${PKG_LICENSES_FILE}"
+
+# Filter out non-license words
+for word in ${LICENSES_FILTER}; do
+    sed -e "/^${word}$/d" -i "${WORK_DIR}/${PKG_LICENSES_FILE}"
+done
+
+log_line "Done!" 1
