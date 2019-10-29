c1e791d68b9c863385a690aa50ae88c91e53d19f
George T Kramer <george.t.kramer@intel.com>
2019-10-29 14:06:16 -0700

    [mixer] Account for different folder structures when fetching bundles
    Bundle sources for an upstream need to go into local-bundles.  These
    will come from the clr-bundles repository.  This repository stores
    bundle definitions in the bundles folder, and cannot be cloned directly
    into the local-bundles folder.  Allow for the folder where the bundle
    definitions live to vary among repos via a config variable
    BUNDLES_REPO_SRC_DIR.
    
    Signed-off-by: George T Kramer <george.t.kramer@intel.com>

--- release/mixer.sh
+++ release/mixer.sh
@@ -35,9 +35,11 @@ sudo_mixer_cmd() {
 }
 
 fetch_bundles() {
-    log_line "Fetching downstream bundles:"
+    log_line "Fetching bundles:"
+    local bundles_dir="${WORK_DIR}/bundles"
+    git clone --quiet "${BUNDLES_REPO}" "${bundles_dir}"
     rm -rf ./local-bundles
-    git clone --quiet "${BUNDLES_REPO}" local-bundles
+    mv "${bundles_dir}/${BUNDLES_REPO_SRC_DIR}" ./local-bundles
     log_line "OK!" 1
 }
 
