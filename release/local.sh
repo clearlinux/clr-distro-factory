5c11e8ebaea087309cb4a091ad378daacd48e4bd
George T Kramer <george.t.kramer@intel.com>
2019-11-26 12:21:55 -0800

    Copy rpms in parallel
    When there are many rpms, need to copy multiple at a time, otherwise
    this process is slow if an upstream.
    
    Signed-off-by: George T Kramer <george.t.kramer@intel.com>

--- release/local.sh
+++ release/local.sh
@@ -20,21 +20,16 @@ SCRIPT_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
 stage "Content Provider - Local Folder"
 
 pkg_list="${WORK_DIR}/${PKG_LIST_TMP}local"
+pkg_list_files="${WORK_DIR}/${PKG_LIST_TMP}local-files"
 true > "${pkg_list}"
 
 if [[ -n ${LOCAL_RPM_DIR} && -d ${LOCAL_RPM_DIR} ]]; then
     log "RPMs Folder" "${LOCAL_RPM_DIR}"
     log_line
 
-    rpms=$(find "${LOCAL_RPM_DIR}/" \
-        -iname "*.rpm" -type f -printf '%f ' 2>/dev/null || true)
-
-    for rpm in ${rpms}; do
-        log_line "Copying '${rpm}':"
-        cp "${LOCAL_RPM_DIR}/${rpm}" "${PKGS_DIR}" 2>/dev/null
-        log_line "OK!" 1
-        basename "$rpm" ".rpm" >> "${pkg_list}"
-    done
+    find "${LOCAL_RPM_DIR}/" -iname "*.rpm" -type f > "${pkg_list_files}"
+    xargs -I {} -a "${pkg_list_files}" cp {} "${PKGS_DIR}"
+    sed -r -e 's|(\w+/)+||' -e 's|^/||' -e 's/\.rpm$//' "${pkg_list_files}" > "${pkg_list}"
 else
     warn "Custom Content Not Found" "'LOCAL_RPM_DIR' not defined."
 fi
